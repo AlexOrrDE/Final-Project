51e5e4bd7ba32eb262117ba5e9a23d015f83d2a2
JoePallister <joepallister@gmail.com>
2023-10-31 16:08:21 +0000

    Made lambda.tf file, that makes a layer and a lambda function and puts them together

---
+++ terraform/iam.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "lambda_role" {
+    name_prefix = "role-${var.lambda_name}"
+    assume_role_policy = jsonencode(
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "sts:AssumeRole"
+                ],
+                "Principal": {
+                    "Service": [
+                        "lambda.amazonaws.com"
+                    ]
+                }
+            }
+        ]
+    })
+}
