c4219189798d442ad9d80a5bacc531dfdcd877d5
JoePallister <joepallister@gmail.com>
2023-11-01 16:13:41 +0000

    No idea

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,18 +4,18 @@ resource "aws_s3_bucket" "code_bucket" {
 }
 
 # Zip up our function
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_file = "${path.module}/../src/ingestion/handler.py"
-  output_path = "${path.module}/../handler.zip"
-}
-
-# Trying to zip directory with each of our python files
 # data "archive_file" "lambda_zip" {
 #   type        = "zip"
-#   source_dir = "../src/ingestion"
-#   output_path = "../handler.zip"
+#   source_file = "${path.module}/../src/ingestion/handler.py"
+#   output_path = "${path.module}/../handler.zip"
 # }
+
+# Trying to zip directory with each of our python files
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir = "../src/ingestion"
+  output_path = "../handler.zip"
+}
 
 # Turn zipped function into s3 object
 resource "aws_s3_object" "lambda_code" {
