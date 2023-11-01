e78db5a71c7b3268d104deb4e03ba698767753e5
RymanAk <Rymanak@icloud.com>
2023-11-01 15:37:15 +0000

    docstring for the retrieve_totesys_credentials function

--- src/ingestion/connection.py
+++ src/ingestion/connection.py
@@ -32,7 +32,28 @@ def connect_to_database():
 
 
 def retrieve_totesys_credentials(secret_name):
-    """INSERT DOC STRING"""
+    """
+    Retrieve database credentials from AWS Secrets Manager.
+
+    This function retrieves database connection credentials from AWS Secrets Manager
+    based on the provided `secret_name`.
+
+    Args:
+        secret_name (str): The name of the secret containing the credentials in AWS Secrets Manager.
+
+    Returns:
+        A dictionary containing the database connection credentials, including:
+            - 'host': The hostname of the database server.
+            - 'port': The port number for the database server.
+            - 'database': The name of the database.
+            - 'user': The username for database authentication.
+            - 'password': The password for database authentication.
+
+    Raises:
+        - InvalidStoredCredentials: If the retrieved credentials do not contain the expected keys
+        ('host', 'port', 'database', 'user', 'password').
+        - JSONDecodeError: If there is an issue with decoding the JSON content.
+    """
 
     secrets_manager = boto3.client("secretsmanager")
     response = secrets_manager.get_secret_value(SecretId=secret_name)
