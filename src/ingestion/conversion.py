d58102a25f068a7bd9a390d8115c922a33122986
RymanAk <Rymanak@icloud.com>
2023-11-02 12:33:03 +0000

    removed print statement

--- src/ingestion/conversion.py
+++ src/ingestion/conversion.py
@@ -10,7 +10,6 @@ def convert_to_csv(table_data):
 
       file_name, csv_data = convert_to_csv(file_data)
     """
-    print(table_data)
     table_name = table_data["table_name"]
     csv_data = pd.DataFrame(table_data["data"]).to_csv(index=False)
     return table_name, csv_data
