fdf417d4c0ba756ef0e5760072d721846b27f90e
Joshua Hibbard <joshcarrick@btinternet.com>
2023-11-03 11:15:49 +0000

    fix import and bucket names

--- test/test_write_to_s3.py
+++ test/test_write_to_s3.py
@@ -1,6 +1,7 @@
-from src.ingestion.convert_to_csv import write_to_s3
+from src.ingestion.write_to_s3 import write_to_s3
 from moto import mock_s3
 from freezegun import freeze_time
+from datetime import datetime
 import pytest
 import boto3
 import os
@@ -28,27 +29,27 @@ def s3_client(aws_credentials):
 def test_write_to_s3_adds_file_to_test_bucket(s3_client):
     """Test to check the write_to_s3 function is able to add the correct item to a bucket"""
     s3_client.create_bucket(
-        Bucket="marble-test-bucket",
+        Bucket="ingestion-data-bucket-marble",
         CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
     )
-    assert "Contents" not in s3_client.list_objects(Bucket="marble-test-bucket")
+    assert "Contents" not in s3_client.list_objects(Bucket="ingestion-data-bucket-marble")
 
     write_to_s3("test-table", "test-data")
 
     assert (
-        len(s3_client.list_objects(Bucket="marble-test-bucket")["Contents"]) == 1
+        len(s3_client.list_objects(Bucket="ingestion-data-bucket-marble")["Contents"]) == 1
     )
 
 
 @freeze_time("2023-01-01")
 def test_write_to_s3_adds_correct_prefix_and_suffix_to_filename_on_upload(s3_client):
     s3_client.create_bucket(
-        Bucket="marble-test-bucket",
+        Bucket="ingestion-data-bucket-marble",
         CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
     )
     write_to_s3("test-table", "test-data")
     assert (
-        s3_client.list_objects(Bucket="marble-test-bucket")["Contents"][0]["Key"]
+        s3_client.list_objects(Bucket="ingestion-data-bucket-marble")["Contents"][0]["Key"]
         == "2023-01-01 00:00:00-test-table.csv"
     )
 
@@ -56,7 +57,7 @@ def test_write_to_s3_adds_correct_prefix_and_suffix_to_filename_on_upload(s3_cli
 def test_should_raise_client_error_if_bucket_does_not_exist(s3_client):
     with raises(ClientError):    
         s3_client.create_bucket(
-                Bucket='not-marble-test-bucket',
+                Bucket='not-ingestion-data-bucket-marble',
                 CreateBucketConfiguration={
                 'LocationConstraint': 'eu-west-2'
             }
@@ -68,7 +69,7 @@ def test_should_raise_client_error_if_bucket_does_not_exist(s3_client):
 def test_should_raise_type_error_if_called_with_incorrect_parameters(s3_client):
     with raises(TypeError):    
         s3_client.create_bucket(
-                Bucket='not-marble-test-bucket',
+                Bucket='not-ingestion-data-bucket-marble',
                 CreateBucketConfiguration={
                 'LocationConstraint': 'eu-west-2'
             }
