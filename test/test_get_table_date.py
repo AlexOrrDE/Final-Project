b3bce31b5b467c7d9f1720c33a5aa3fa170d10a0
Joshua Hibbard <joshcarrick@btinternet.com>
2023-11-06 12:32:27 +0000

    Removed most reference to region

--- test/test_get_table_date.py
+++ test/test_get_table_date.py
@@ -11,10 +11,9 @@ def aws_credentials():
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
-    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
 
 @pytest.fixture(scope="function")
 def s3(aws_credentials):
     with mock_s3:
-        yield boto3.client("s3", region_name="eu-west-2")
+        yield boto3.client("s3")
--- test/test_write_to_s3.py
+++ test/test_write_to_s3.py
@@ -15,13 +15,12 @@ def aws_credentials():
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
-    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
 
 @pytest.fixture(scope="function")
 def s3_client(aws_credentials):
     with mock_s3():
-        yield boto3.client("s3", region_name="eu-west-2")
+        yield boto3.client("s3")
 
 
 @freeze_time("2023-01-01")
