9a99b04af7f6d6bdbdf0320a183a9d9b37a71a04
Joshua Hibbard <joshcarrick@btinternet.com>
2023-11-08 15:58:25 +0000

    Complete func, tests, and pep8

--- src/processing/table_merge.py
+++ src/processing/table_merge.py
@@ -4,12 +4,28 @@ import pandas as pd
 import boto3
 from botocore.exceptions import ClientError
 
+
 def table_merge(source_df):
+    """Merges the source dataframe with the appropriate secondary table.
+
+    Args:
+        source_df: The dataframe taken from the target csv file.
+    Returns:
+        source_df: If the table doesn't need to be merged,
+        returns the input dataframe
+        merged_table: If it does need to be merged,
+        returns the two dataframes joined on the appropriate key
+    Raises:
+        RuntimeError: Generic error handling
+        KeyError: If the source_df doesn't correlate
+        to any files in the bucket
+        ClientError: If AWS encounters any errors
+    """
     try:
         table_1 = source_df.columns[0]
         table_dict = {
-        'counterparty_id': ['address', 'legal_address_id'],
-        'staff_id': ['department', 'department_id']
+            'counterparty_id': ['address', 'legal_address_id'],
+            'staff_id': ['department', 'department_id']
         }
         if table_1 in table_dict:
             # defines the table names and keys to merge on
@@ -20,29 +36,37 @@ def table_merge(source_df):
             # call to s3 client, lists objects in data bucket
             s3 = boto3.client('s3')
             response = s3.list_objects(Bucket='ingestion-data-bucket-marble')
+            second_tables = [
+                obj['Key'] for obj in response['Contents']
+                if table_2 in obj['Key']]
 
-            # TODO add in capability to search for the most recent update
-            second_tables = [obj['Key'] for obj in response['Contents'] if table_2 in obj['Key']]
-            second_table_data = s3.get_object(Bucket='ingestion-data-bucket-marble', Key=f'{second_tables[0]}')
+            # get latest version of file
+            latest_table = max(second_tables)
 
             # reads the data from a given address table in s3
-            read_secondary_data = second_table_data['Body'].read().decode('utf-8')
+            second_table_data = s3.get_object(
+                Bucket='ingestion-data-bucket-marble', Key=latest_table)
+            read_secondary_data = (
+                second_table_data['Body'].read().decode('utf-8'))
             secondary_file = io.StringIO(read_secondary_data)
 
             # converts the secondary table to a dataframe
             second_source_df = pd.read_csv(secondary_file, index_col=False)
 
             # merges the tables by the keys needed
-            merged_table = pd.merge(source_df, second_source_df, left_on=f'{table_1_key}', right_on=f'{table_2_key}')
+            merged_table = pd.merge(
+                source_df, second_source_df,
+                left_on=f'{table_1_key}', right_on=f'{table_2_key}')
             pd.set_option('display.max_columns', None)
-            logging.info('Tables %s and %s have been merged.', table_1, table_2)
+            logging.info(
+                'Tables %s and %s have been merged.', table_1, table_2)
             return merged_table
         else:
             logging.info('No need to merge')
             return source_df
-    except RuntimeError as e:
-        raise(e)
-    except KeyError as ke:
-        raise(ke)
-    except ClientError as ce:
-        raise(ce)
+    except RuntimeError:
+        raise
+    except KeyError:
+        raise
+    except ClientError:
+        raise
--- test/test_table_merge.py
+++ test/test_table_merge.py
@@ -37,157 +37,170 @@ def create_bucket(s3_client):
 @pytest.fixture(scope="function")
 def create_counterparty_data():
     yield {
-    'table_name': 'counterparty',
-    'data': [{
-        'counterparty_id': 1,
-        'counterparty_legal_name': 'cp_1',
-        'legal_address_id': 1,
-        'commercial_contact': 'test',
-        'delivery_contact': 'test',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')},{
-        'counterparty_id': 2,
-        'counterparty_legal_name': 'cp_2',
-        'legal_address_id': 3,
-        'commercial_contact': 'test',
-        'delivery_contact': 'test',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')},{
-        'counterparty_id': 3,       
-        'counterparty_legal_name': 'cp_3',
-        'legal_address_id': 2,
-        'commercial_contact': 'test',
-        'delivery_contact': 'test',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')}]
-        }
-
-
-@pytest.fixture(scope="function")
-def create_updated_counterparty_data():
-    yield {
-    'table_name': 'counterparty',
-    'data': [{
-        'counterparty_id': 1,
-        'counterparty_legal_name': 'cp_new',
-        'legal_address_id': 1,
-        'commercial_contact': 'test',
-        'delivery_contact': 'test',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')},{
-        'counterparty_id': 2,
-        'counterparty_legal_name': 'cp_2',
-        'legal_address_id': 3,
-        'commercial_contact': 'test',
-        'delivery_contact': 'test',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')},{
-        'counterparty_id': 3,       
-        'counterparty_legal_name': 'cp_3',
-        'legal_address_id': 2,
-        'commercial_contact': 'test',
-        'delivery_contact': 'test',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')}]
-        }
+        'table_name': 'counterparty',
+        'data': [{
+            'counterparty_id': 1,
+            'counterparty_legal_name': 'cp_1',
+            'legal_address_id': 1,
+            'commercial_contact': 'test',
+            'delivery_contact': 'test',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}, {
+            'counterparty_id': 2,
+            'counterparty_legal_name': 'cp_2',
+            'legal_address_id': 3,
+            'commercial_contact': 'test',
+            'delivery_contact': 'test',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}, {
+            'counterparty_id': 3,
+            'counterparty_legal_name': 'cp_3',
+            'legal_address_id': 2,
+            'commercial_contact': 'test',
+            'delivery_contact': 'test',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}]
+            }
 
 
 @pytest.fixture(scope="function")
 def create_staff_data():
     yield {
-    'table_name': 'counterparty',
-    'data': [{
-        'staff_id': 1,
-        'first_name': 'staff_name_1',
-        'last_name': 'staff_surname_1',
-        'department_id': 1,
-        'email_address': 'test@test.com',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')},{
-        'staff_id': 1,
-        'first_name': 'staff_name_2',
-        'last_name': 'staff_surname_2',
-        'department_id': 2,
-        'email_address': 'test@test.com',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')},{
-        'staff_id': 1,
-        'first_name': 'staff_name_3',
-        'last_name': 'staff_surname_3',
-        'department_id': 3,
-        'email_address': 'test@test.com',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')}]
-        }
+        'table_name': 'counterparty',
+        'data': [{
+            'staff_id': 1,
+            'first_name': 'staff_name_1',
+            'last_name': 'staff_surname_1',
+            'department_id': 1,
+            'email_address': 'test@test.com',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}, {
+            'staff_id': 1,
+            'first_name': 'staff_name_2',
+            'last_name': 'staff_surname_2',
+            'department_id': 2,
+            'email_address': 'test@test.com',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}, {
+            'staff_id': 1,
+            'first_name': 'staff_name_3',
+            'last_name': 'staff_surname_3',
+            'department_id': 3,
+            'email_address': 'test@test.com',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}]
+            }
 
 
 @pytest.fixture(scope="function")
 def create_department_data():
     yield {
-    'table_name': 'counterparty',
-    'data': [{
-        'department_id': 1,
-        'department_name': 'dept_1',
-        'location': 1,
-        'manager': 'test',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')},{
-        'department_id': 2,
-        'department_name': 'dept_2',
-        'location': 3,
-        'manager': 'test',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')},{
-        'department_id': 3,       
-        'department_name': 'dept_3',
-        'location': 2,
-        'manager': 'test',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')}]
-        }
+        'table_name': 'counterparty',
+        'data': [{
+            'department_id': 1,
+            'department_name': 'dept_1',
+            'location': 1,
+            'manager': 'test',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}, {
+            'department_id': 2,
+            'department_name': 'dept_2',
+            'location': 3,
+            'manager': 'test',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}, {
+            'department_id': 3,
+            'department_name': 'dept_3',
+            'location': 2,
+            'manager': 'test',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}]
+            }
 
 
 @pytest.fixture(scope="function")
 def create_address_data():
     yield {
-    'table_name': 'address',
-    'data': [{
-        'address_id': 1,
-        'address_line_1': 'add_1',
-        'address_line_2': 'add_1_2',
-        'district': 'test_district',
-        'city': 'test_city',
-        'postal_code': 'test_post',
-        'country': 'test_country',
-        'phone': 'test_phone',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')},
-        {
-        'address_id': 2,
-        'address_line_1': 'add_2',
-        'address_line_2': 'add_2_2',
-        'district': 'test_district',
-        'city': 'test_city',
-        'postal_code': 'test_post',
-        'country': 'test_country',
-        'phone': 'test_phone',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')},
-        {
-        'address_id': 3,
-        'address_line_1': 'add_3',
-        'address_line_2': 'add_3_3',
-        'district': 'test_district',
-        'city': 'test_city',
-        'postal_code': 'test_post',
-        'country': 'test_country',
-        'phone': 'test_phone',
-        'created_at': Timestamp('2023-01-01 00:00:00.000000'),
-        'last_updated': Timestamp('2023-01-01 00:00:00.000000')}]
-        }
+        'table_name': 'address',
+        'data': [{
+            'address_id': 1,
+            'address_line_1': 'add_1',
+            'address_line_2': 'add_1_2',
+            'district': 'test_district',
+            'city': 'test_city',
+            'postal_code': 'test_post',
+            'country': 'test_country',
+            'phone': 'test_phone',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')},
+            {
+            'address_id': 2,
+            'address_line_1': 'add_2',
+            'address_line_2': 'add_2_2',
+            'district': 'test_district',
+            'city': 'test_city',
+            'postal_code': 'test_post',
+            'country': 'test_country',
+            'phone': 'test_phone',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')},
+            {
+            'address_id': 3,
+            'address_line_1': 'add_3',
+            'address_line_2': 'add_3_3',
+            'district': 'test_district',
+            'city': 'test_city',
+            'postal_code': 'test_post',
+            'country': 'test_country',
+            'phone': 'test_phone',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}]
+            }
 
 
-def test_table_merge_combines_counterparty_and_address(create_bucket, s3_client, create_counterparty_data,create_address_data):
+@pytest.fixture(scope="function")
+def create_updated_address_data():
+    yield {
+        'table_name': 'address',
+        'data': [{
+            'address_id': 1,
+            'address_line_1': 'updated_add_1',
+            'address_line_2': 'add_1_2',
+            'district': 'test_district',
+            'city': 'test_city',
+            'postal_code': 'test_post',
+            'country': 'test_country',
+            'phone': 'test_phone',
+            'created_at': Timestamp('2023-01-02 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-02 00:00:00.000000')},
+            {
+            'address_id': 2,
+            'address_line_1': 'add_2',
+            'address_line_2': 'add_2_2',
+            'district': 'test_district',
+            'city': 'test_city',
+            'postal_code': 'test_post',
+            'country': 'test_country',
+            'phone': 'test_phone',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')},
+            {
+            'address_id': 3,
+            'address_line_1': 'add_3',
+            'address_line_2': 'add_3_3',
+            'district': 'test_district',
+            'city': 'test_city',
+            'postal_code': 'test_post',
+            'country': 'test_country',
+            'phone': 'test_phone',
+            'created_at': Timestamp('2023-01-01 00:00:00.000000'),
+            'last_updated': Timestamp('2023-01-01 00:00:00.000000')}]
+            }
+
+
+def test_table_merge_combines_counterparty_and_address(
+        create_bucket, s3_client, create_counterparty_data,
+        create_address_data):
     cp_name, cp_csv = convert_to_csv(create_counterparty_data)
     ad_name, ad_csv = convert_to_csv(create_address_data)
 
@@ -207,7 +220,9 @@ def test_table_merge_combines_counterparty_and_address(create_bucket, s3_client,
     assert result is not source_df
 
 
-def test_table_merge_combines_staff_and_department(create_bucket, s3_client, create_staff_data,create_department_data):
+def test_table_merge_combines_staff_and_department(
+        create_bucket, s3_client, create_staff_data,
+        create_department_data):
     staff_name, staff_csv = convert_to_csv(create_staff_data)
     dept_name, dept_csv = convert_to_csv(create_department_data)
 
@@ -227,7 +242,9 @@ def test_table_merge_combines_staff_and_department(create_bucket, s3_client, cre
     assert result is not source_df
 
 
-def test_table_merge_returns_table_if_no_merge_needed(create_bucket, s3_client, create_department_data):
+def test_table_merge_returns_table_if_no_merge_needed(
+        create_bucket, s3_client,
+        create_department_data):
     dept_name, dept_csv = convert_to_csv(create_department_data)
     s3_client.put_object(
         Bucket="ingestion-data-bucket-marble",
@@ -239,32 +256,37 @@ def test_table_merge_returns_table_if_no_merge_needed(create_bucket, s3_client, 
     assert result is source_df
 
 
-def xtest_table_merge_gets_latest_version_of_file(
-    create_bucket, s3_client, create_counterparty_data, create_address_data, create_updated_counterparty_data):
+def test_table_merge_gets_latest_version_of_file(
+        create_bucket, s3_client, create_counterparty_data,
+        create_address_data, create_updated_address_data):
     cp_name, cp_csv = convert_to_csv(create_counterparty_data)
     ad_name, ad_csv = convert_to_csv(create_address_data)
-    updatedcp_name, updatedcp_csv = convert_to_csv(create_updated_counterparty_data)
+    updated_ad_name, updated_ad_csv = convert_to_csv(
+        create_updated_address_data)
 
     s3_client.put_object(
         Bucket="ingestion-data-bucket-marble",
         Key="2023/01/01/counterparty/00:00.csv",
         Body=cp_csv)
+
     s3_client.put_object(
         Bucket="ingestion-data-bucket-marble",
-        Key="2023/01/02/counterparty/00:00.csv",
-        Body=cp_csv)
+        Key="2023/01/01/address/00:00.csv",
+        Body=ad_csv)
     s3_client.put_object(
         Bucket="ingestion-data-bucket-marble",
-        Key="2023/01/01/address/00:00clear.csv",
-        Body=updatedcp_csv)
-    
+        Key="2023/01/01/address/00:01.csv",
+        Body=updated_ad_csv)
+
     buffer = io.StringIO(cp_csv)
     source_df = pd.read_csv(buffer)
     result = table_merge(source_df)
-    print (result.at[0, 'counterparty_name'])
-    assert result.at[0, 'counterparty_name'] == 'cp_new'
+    print(result)
+    assert result.at[0, 'address_line_1'] == 'updated_add_1'
 
-def test_table_merge_raises_key_error_if_table_is_not_in_bucket(create_staff_data, create_bucket, s3_client):
+
+def test_table_merge_raises_key_error_if_table_is_not_in_bucket(
+        create_staff_data, create_bucket, s3_client):
     staff_name, staff_csv = convert_to_csv(create_staff_data)
     buffer = io.StringIO(staff_csv)
     source_df = pd.read_csv(buffer)
