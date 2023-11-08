4db4351161b72891cbfba726723e3ab0046bce6e
Shabbir1246 <mydatabackupac@gmail.com>
2023-11-08 08:55:38 +0000

    create_dim_date in development

--- src/processing/create_dim_date.py
+++ src/processing/create_dim_date.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import datetime as datetime
-import boto3
-import io
 
 def create_dim_date(start, end):
     given_date = datetime.datetime.strptime(start, "%Y/%m/%d") 
@@ -28,17 +26,17 @@ def create_dim_date(start, end):
     num_days = (datetime.datetime.strptime(end, "%Y/%m/%d") - datetime.datetime.strptime(start, "%Y/%m/%d")).days -1
     for i in range(num_days):
         given_date = given_date + datetime.timedelta(days=1)
-        df_date = df_date.append({
-            "date_id": [given_date.strftime("%Y%m%d")],
-            "date": [given_date.strftime("%Y/%m/%d")],
-            "year": [given_date.year],
-            "month": [given_date.month],
-            "day": [given_date.day],
-            "day_of_week": [given_date.weekday()],
-            "day_name": [given_date.strftime("%A")],
-            "month_name": [given_date.strftime("%B")],
-            "quarter": [(given_date.month-1)//3 + 1]
-        }, ignore_index=True)
+        df_date.loc[len(df_date)] = {
+            "date_id": given_date.strftime("%Y%m%d"),
+            "date": given_date.strftime("%Y/%m/%d"),
+            "year": given_date.year,
+            "month": given_date.month,
+            "day": given_date.day,
+            "day_of_week": given_date.weekday(),
+            "day_name": given_date.strftime("%A"),
+            "month_name": given_date.strftime("%B"),
+            "quarter": (given_date.month-1)//3 + 1
+        }
         
     # print (num_days, year, month, day, day_of_week, day_name, month_name, quarter)
     return df_date
