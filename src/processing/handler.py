1f18b6f9b6864e7b2fac52fce1b57c755de92fb5
JoePallister <joepallister@gmail.com>
2023-11-08 16:31:03 +0000

    Continued mapping out handler

--- src/processing/handler.py
+++ src/processing/handler.py
@@ -2,19 +2,33 @@ import boto3
 import io
 import pandas as pd
 from write_to_s3 import write_to_s3
+# from dimensions-fact.dim_counterparty import create_dim_counterparty
+# from dimensions-fact.dim_currency import create_dim_currency
+# from dimensions-fact.dim_date import create_dim_date
+# from dimensions-fact.dim_design import create_dim_design
+# from dimensions-fact.dim_location import create_dim_location
+# from dimensions-fact.dim_staff import create_dim_staff
+# from dimensions-fact.fact-sales-order import create_fact_sales_order
+
+function_dict = {"counterparty" : create_dim_counterparty, "currency" : create_dim_currency, "date" : create_dim_date, "design" : create_dim_design, "location" : create_dim_location, "staff" : create_dim_staff, "sales_order" : create_fact_sales_order}
 
 def handler(event, context):
     s3 = boto3.client('s3')
     for table_name in event:
+        
         key = event[table_name]
         update_data = s3.get_object(Bucket='ingestion-data-bucket-marble', Key=key)
         read_update_data = update_data['Body'].read().decode('utf-8')
         update_file = io.StringIO(read_update_data)
         df = pd.read_csv(update_file, index_col=False)
         # Josh has this
-        # merge_tables(df)
+        merged = merge_tables(df)
         # Alex, Ryman, Shabbir
-        # alex_rymans_function(merge_tables(df))
+        try:
+            our_func = function_dict[table_name]
+            result = our_func(merged)
+        except:
+            pass
         # Alex has this
-        # returned_parquet = convert_to_parquet()
-        # write_to_s3(key, returned_parquet)
+        returned_parquet = convert_to_parquet(result)
+        write_to_s3(key, returned_parquet)
