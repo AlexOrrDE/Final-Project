ed7200f2796c1331b6746a477cb77048c2c56a3e
Alex Orr <134069558+RadGuav@users.noreply.github.com>
2023-11-08 15:00:25 +0000

    dim tables work

---
+++ src/processing/dimensions-fact/dim_design.py
@@ -0,0 +1,6 @@
+def create_dim_design(design_df):
+    columns_to_keep = ["design_id", "design_name", "file_location", "file_name"]
+
+    dim_design_df = design_df[columns_to_keep]
+
+    return dim_design_df
