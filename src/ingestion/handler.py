22d4f511ba0a05b6304f10d98717045e46b2b93a
JoePallister <joepallister@gmail.com>
2023-11-09 13:59:02 +0000

    changed which python file runs for second lambda

--- src/ingestion/handler.py
+++ src/ingestion/handler.py
@@ -67,6 +67,7 @@ def handler(event, context):
         if not update:
             logging.info("No need to update")
         else:
+            logging.info("Calling next lambda")
             lambda_client = boto3.client('lambda')
             json_tables = json.dumps(updated_tables)
             lambda_client.invoke(FunctionName='processing_handler', InvocationType='Event', Payload=json_tables)
