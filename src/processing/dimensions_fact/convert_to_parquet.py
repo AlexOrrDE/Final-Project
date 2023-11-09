d0ab03d322a566aa76c53df1fb14f76b50385bda
Shabbir1246 <mydatabackupac@gmail.com>
2023-11-09 12:02:36 +0000

    adds convert_to_parquet.py

---
+++ src/processing/dimensions_fact/convert_to_parquet.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import logging
+
+def convert_to_parquet(df):
+    """Converts a pandas dataframe to parquet format
+    
+    Typical usage example:
+    
+        convert_to_parquet(df)
+    """
+    try:
+        return df.to_parquet()
+    except KeyError as ke:
+        logging.error("Error occured in convert_to_parquet")
+        raise ke
