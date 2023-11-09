65c6eada3284d0ed017d44d28f705aee3d1e0370
JoePallister <joepallister@gmail.com>
2023-11-09 11:16:07 +0000

    added error handling test to test_dim_loc

--- test/test_dimension_fact/test_dim_location.py
+++ test/test_dimension_fact/test_dim_location.py
@@ -1,10 +1,19 @@
 from src.processing.dimensions_fact.dim_location import create_dim_location
 import pytest
 import pandas as pd
+from pytest import raises
 
 @pytest.fixture(scope="function")
 def create_address_data():
     data = {'address_id' : [1,2], 'address_line_1' : ['test_address_1', 'test_address_2'], 'address_line_2' : ['test_second_address_1', 'test_second_address_2'], 'district' : ['test_dist_1', 'test_dict_2'], 'city' : ['test_city_1', 'test_city_2'], 'postal_code' : ['test_postal_1', 'test_postal_2'], 'country' : ['test_country_1', 'tst_country_2'], 'phone': ['059695', '284949'], 'created_at' : ['2022-11-03 14:20:49.962', '2023-06-03 16:20:49.962']}
+
+    df = pd.DataFrame(data=data)
+
+    yield df
+
+@pytest.fixture(scope="function")
+def create_bad_address_data():
+    data = {'fake_column_name' : [1,2], 'address_line_1' : ['test_address_1', 'test_address_2'], 'address_line_2' : ['test_second_address_1', 'test_second_address_2'], 'district' : ['test_dist_1', 'test_dict_2'], 'city' : ['test_city_1', 'test_city_2'], 'postal_code' : ['test_postal_1', 'test_postal_2'], 'country' : ['test_country_1', 'tst_country_2'], 'phone': ['059695', '284949'], 'created_at' : ['2022-11-03 14:20:49.962', '2023-06-03 16:20:49.962']}
 
     df = pd.DataFrame(data=data)
 
@@ -33,3 +42,6 @@ def test_dim_design_doesnt_change_data_in_columns_it_keeps(create_address_data):
     for idx, elt in enumerate(row_one):
         assert elt == expected_row_one[idx]
 
+def test_should_raise_error_if_given_incorrect_dataframe():
+    with raises(AttributeError):
+        create_dim_location(create_bad_address_data)
