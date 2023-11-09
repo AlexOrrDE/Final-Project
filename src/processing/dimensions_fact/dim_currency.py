b2a6f58fd128282718fb264a5f63c85d1716ead2
RymanAk <Rymanak@icloud.com>
2023-11-09 11:50:33 +0000

    dim_currency testing

--- src/processing/dimensions_fact/dim_currency.py
+++ src/processing/dimensions_fact/dim_currency.py
@@ -3,6 +3,19 @@ import logging
 
 
 def create_dim_currency(currency_df):
+    """
+    Creates a dimension DataFrame for currencies with a format that matches the table in the warehouse
+
+    Args:
+        currency_df (pd.DataFrame): Input DataFrame
+        with the columns : 'currency_id', 'currency_name', 'created_at' and last_updated
+
+    Returns:
+        pd.DataFrame: DataFrame with 'currency_id', 'currency_code', and 'currency_name'
+
+    Raises:
+        KeyError: If the input dataframe does not have the expected format.
+    """
     try:
         currency_df["currency_name"] = currency_df["currency_code"].apply(
             lambda code: ccy.currency(code).name
@@ -15,6 +28,3 @@ def create_dim_currency(currency_df):
         raise ke
 
     return dim_currency_df
-
-
-
