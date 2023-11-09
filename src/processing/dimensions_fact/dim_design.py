1dd5f6b9c568801106b119a36528156200a8ca1b
Charles Larcher <Carlos@DSLL.local>
2023-11-09 10:51:54 +0000

    testing dim design

--- src/processing/dimensions_fact/dim_design.py
+++ src/processing/dimensions_fact/dim_design.py
@@ -1,6 +1,12 @@
+import logging 
+
 def create_dim_design(design_df):
-    columns_to_keep = ["design_id", "design_name", "file_location", "file_name"]
+    try:
+        columns_to_keep = ["design_id", "design_name", "file_location", "file_name"]
 
-    dim_design_df = design_df[columns_to_keep]
+        dim_design_df = design_df[columns_to_keep]
 
-    return dim_design_df
+        return dim_design_df
+    except KeyError as ke:
+        logging.error("Error occured in create_dim_design")
+        raise ke
