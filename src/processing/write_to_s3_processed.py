d442bf5199056db4c68157cd14698aaacf368d93
Shabbir1246 <mydatabackupac@gmail.com>
2023-11-09 10:59:59 +0000

    working on s3_write

--- src/processing/write_to_s3_processed.py
+++ src/processing/write_to_s3_processed.py
@@ -19,9 +19,33 @@ def write_to_s3_processed(df, s3_bucket, s3_key):
     
     # write to the given processed s3 bucket
     boto3.client("s3").put_object(
-      Bucket=s3_bucket, Key=timestamped_key, Body=table)
+      Bucket=s3_bucket, s3_key=timestamped_key, Body=table)
     
     # pq.write_table(table, f"s3://{s3_bucket}/{timestamped_key}")
+    return s3_key
                    
+
+
+# def write_to_s3(
+#         key,
+#         parquet,
+#         bucket_name="processing-data-bucket-marble"):
+#     """Uploads files to AWS s3 bucket.
+
+#     Puts objects in s3 bucket with a timestamp in the filename.
+
+#     Typical usage example:
+
+#       write_to_s3(file_name, file_data)
+#     """
+
+#     key_without_csv = key[:-4]
+
+#     s3_key = f"{key}.parquet"
+#     boto3.client("s3").put_object(
+#       Bucket=bucket_name, Key=s3_key, Body=parquet
+#     )
+
+#     return s3_key
 
                    
