9fdeeca42adec27c730188ef5136f377187e150b
RymanAk <Rymanak@icloud.com>
2023-11-09 11:20:52 +0000

    currency test

--- src/processing/dimensions_fact/dim_currency.py
+++ src/processing/dimensions_fact/dim_currency.py
@@ -1,12 +1,20 @@
 import ccy
+import logging
 
 
 def create_dim_currency(currency_df):
-    currency_df["currency_name"] = currency_df["currency_code"].apply(
-        lambda code: ccy.currency(code).name
-    )
-    columns_to_keep = ["currency_id", "currency_code", "currency_name"]
+    try:
+        currency_df["currency_name"] = currency_df["currency_code"].apply(
+            lambda code: ccy.currency(code).name
+        )
+        columns_to_keep = ["currency_id", "currency_code", "currency_name"]
 
-    dim_currency_df = currency_df[columns_to_keep]
+        dim_currency_df = currency_df[columns_to_keep]
+    except KeyError as ke:
+        logging.error("Error occured in create_dim_currency")
+        raise ke
 
     return dim_currency_df
+
+
+
--- test/test_dimension_fact/test_dim_currency.py
+++ test/test_dimension_fact/test_dim_currency.py
@@ -1,8 +1,22 @@
 from src.processing.dimensions_fact.dim_currency import create_dim_currency
 import pandas as pd
+from pytest import raises
 
 
-def test_create_dim_currency():
+def test_create_dim_currency_returns_a_pandas_dataframe():
+    test_data = {
+        "currency_id":[1],
+        "currency_code":["GBP"],
+        "created_at":["2023-11-09 00:00:00"],
+        "last_updated":["2023-11-08 00:00:00"]
+    }
+    
+    test_df = pd.DataFrame(test_data)
+    result_df = create_dim_currency(test_df)
+
+    assert isinstance(result_df, pd.core.frame.DataFrame)
+
+def test_create_dim_currency_returns_correct_columns():
     test_data = {
         "currency_id":[1,2],
         "currency_code":["USD", "GBP"],
@@ -17,7 +31,13 @@ def test_create_dim_currency():
 
     test_df = pd.DataFrame(test_data)
 
-    output_df = create_dim_currency(test_df)
+    result_df = create_dim_currency(test_df)
 
 
-    pd.testing.assert_frame_equal(output_df, pd.DataFrame(expected_result))
+    pd.testing.assert_frame_equal(result_df, pd.DataFrame(expected_result))
+
+def test_should_raise_error_if_given_incorrect_dataframe():
+    with raises(KeyError):
+        incorrect_values = [[1, 'Fahey and Sons', 15, 'Micheal Toy', 'Mrs. Lucy Runolfsdottir', '2022-11-03 14:20:51.563', '2022-11-03 14:20:51.563']]
+
+        create_dim_currency(pd.DataFrame(incorrect_values))
