799813f5ffa3466e8da556f13c22ade8cddc5d15
Charles Larcher <Carlos@DSLL.local>
2023-11-09 11:36:00 +0000

    added errors handling and tests for dim counterparty

--- src/processing/dimensions_fact/dim_counterparty.py
+++ src/processing/dimensions_fact/dim_counterparty.py
@@ -1,28 +1,35 @@
+import logging
+
 def create_dim_counterparty(cp_merge_df):
-    column_name_mapping = {
-        "address_line_1": "counterparty_legal_address_line_1",
-        "address_line_2": "counterparty_legal_address_line_2",
-        "district": "counterparty_legal_district",
-        "city": "counterparty_legal_city",
-        "postal_code": "counterparty_legal_postal_code",
-        "country": "counterparty_legal_country",
-        "phone": "counterparty_legal_phone-number",
-    }
+    try:
+        column_name_mapping = {
+            "address_line_1": "counterparty_legal_address_line_1",
+            "address_line_2": "counterparty_legal_address_line_2",
+            "district": "counterparty_legal_district",
+            "city": "counterparty_legal_city",
+            "postal_code": "counterparty_legal_postal_code",
+            "country": "counterparty_legal_country",
+            "phone": "counterparty_legal_phone_number",
+        }
 
-    cp_merge_df.rename(columns=column_name_mapping, inplace=True)
+        cp_merge_df.rename(columns=column_name_mapping, inplace=True)
 
-    columns_to_keep = [
-        "counterparty_id",
-        "counterparty_legal_name",
-        "counterparty_legal_address_line_1",
-        "counterparty_legal_address_line_2",
-        "counterparty_legal_district",
-        "counterparty_legal_city",
-        "counterparty_legal_postal_code",
-        "counterparty_legal_country",
-        "counterparty_legal_phone-number",
-    ]
+        columns_to_keep = [
+            "counterparty_id",
+            "counterparty_legal_name",
+            "counterparty_legal_address_line_1",
+            "counterparty_legal_address_line_2",
+            "counterparty_legal_district",
+            "counterparty_legal_city",
+            "counterparty_legal_postal_code",
+            "counterparty_legal_country",
+            "counterparty_legal_phone_number",
+        ]
 
-    dim_counterparty_df = cp_merge_df[columns_to_keep]
+        dim_counterparty_df = cp_merge_df[columns_to_keep]
 
-    return dim_counterparty_df
+        return dim_counterparty_df
+    except KeyError as ke:
+        logging.error("Error occured in create_dim_counterparty")
+        raise ke
+
