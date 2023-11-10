a2eadee9e1b2e1315cbb819f6a86fb520b672736
JoePallister <joepallister@gmail.com>
2023-11-10 11:25:39 +0000

    Added to concurrency=1 to second lambda. Will this cause issues with the first update, if the first pull is too slow? Delayed eventbridge trigger. Added logs to both handlers

--- src/processing/handler.py
+++ src/processing/handler.py
@@ -21,16 +21,29 @@ function_dict = {
     "currency": create_dim_currency,
     "date": create_dim_date,
     "design": create_dim_design,
-    "location": create_dim_location,
+    "address": create_dim_location,
     "staff": create_dim_staff,
     "sales_order": create_fact_sales_order}
 
 
 def handler(event, context):
+    logging.info("Processing tables:")
+    logging.info(event)
     s3 = boto3.client('s3')
+    s3_resource = boto3.resource("s3")
+    bucket = s3_resource.Bucket("ingestion-data-bucket-marble")
+    
+    check_dim_date = list(bucket.objects.all())
+    if "dim_date" not in check_dim_date:
+        our_func = function_dict["date"]
+        result = our_func()
+        returned_parquet = convert_to_parquet(result)
+        write_to_s3("dim_date", returned_parquet)
+    
     for table_name in event:
+        logging.info("Current table is:")
+        logging.info(table_name)
         if table_name in function_dict:
-            logging.info(table_name)
             key = event[table_name]
             update_data = s3.get_object(
                 Bucket='ingestion-data-bucket-marble', Key=key)
@@ -40,6 +53,10 @@ def handler(event, context):
             merged = table_merge(df)
             our_func = function_dict[table_name]
             result = our_func(merged)
-            logging.info(result)
+            key = key[:-4]
             returned_parquet = convert_to_parquet(result)
+            if "address" in key:
+                key = key.replace("address", "location")
+            logging.info("Writing file:")
+            logging.info(key)
             write_to_s3(key, returned_parquet)
