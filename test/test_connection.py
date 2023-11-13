09b047c6fdf3b0bb15c7f7a98bf9ccb9c0c774f4
Joshua Hibbard <joshcarrick@btinternet.com>
2023-11-13 15:37:42 +0000

    Fix test_connection to use correct function

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,6 +1,6 @@
 import pytest
 from src.ingestion.connection import (
-    retrieve_totesys_credentials,
+    retrieve_credentials,
     InvalidStoredCredentials,
     connect_to_database,
 )
@@ -27,8 +27,8 @@ def secrets_client(aws_credentials):
         yield boto3.client("secretsmanager")
 
 
-def test_retrieve_totesys_credentials_returns_dictionary(secrets_client):
-    """check retrieve_totesys_credentials function always return
+def test_retrieve_credentials_returns_dictionary(secrets_client):
+    """check retrieve_credentials function always return
     a dictionary when secret stored in a valid json format"""
 
     secrets_client.create_secret(
@@ -43,7 +43,7 @@ def test_retrieve_totesys_credentials_returns_dictionary(secrets_client):
                         }
                         """,
     )
-    output = retrieve_totesys_credentials("Totesys-Credentials")
+    output = retrieve_credentials("Totesys-Credentials")
     assert isinstance(output, dict)
     assert (sorted(list(output))) == [
         "database", "host", "password", "port", "user"
@@ -51,7 +51,7 @@ def test_retrieve_totesys_credentials_returns_dictionary(secrets_client):
 
 
 def test_retrieve_credentials_returns_error_when_json_invalid(secrets_client):
-    """check retrieve_totesys_credentials function returns
+    """check retrieve_credentials function returns
     a json.JSONDecodeError when secret stored with invalid json format.
     Example no key-value format"""
 
@@ -64,11 +64,11 @@ def test_retrieve_credentials_returns_error_when_json_invalid(secrets_client):
                         """,
     )
     with pytest.raises(json.JSONDecodeError):
-        retrieve_totesys_credentials("Totesys-Credentials")
+        retrieve_credentials("Totesys-Credentials")
 
 
 def test_retrieve_credentials_throws_InvalidCredentials_error(secrets_client):
-    """check retrieve_totesys_credentials function returns
+    """check retrieve_credentials function returns
     InvalidStoredCredentials error when secret
     stored doesn't have all the required to connect.
     Example when the json is missing a required keys
@@ -85,7 +85,7 @@ def test_retrieve_credentials_throws_InvalidCredentials_error(secrets_client):
     )
 
     with pytest.raises(InvalidStoredCredentials):
-        retrieve_totesys_credentials("Totesys-Credentials")
+        retrieve_credentials("Totesys-Credentials")
 
 
 def test_connection_throws_InterfaceError_when_cannot_connect_to_database(
