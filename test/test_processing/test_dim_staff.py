440c960900161407795e4e9c6391ff8866a8233e
Joshua Hibbard <joshcarrick@btinternet.com>
2023-11-14 13:26:32 +0000

    pep8 compliance

--- test/test_processing/test_dim_staff.py
+++ test/test_processing/test_dim_staff.py
@@ -5,8 +5,36 @@ from src.processing.dimensions_fact.dim_staff import create_dim_staff
 
 @pytest.fixture
 def table_data():
-    column_headers = ['staff_id', 'first_name', 'last_name', 'department_id', 'email_address', 'department_name', 'location', 'manager', 'created_at', 'last_updated']
-    values = [[1, 'test_first_name', 'test_last_name', 'test_department_id', 'test_email', 'test_dept_name', 'test_location','test_manager', '2023-11-09 00:00:00', '2023-11-09 00:00:00'], [2, 'test_first_name2', 'test_last_name2', 'test_department_id2', 'test_email2', 'test_dept_name2', 'test_location2', 'test_manager2', '2023-11-09 00:00:00', '2023-11-09 00:00:00']]
+    column_headers = ['staff_id',
+                      'first_name',
+                      'last_name',
+                      'department_id',
+                      'email_address',
+                      'department_name',
+                      'location',
+                      'manager',
+                      'created_at',
+                      'last_updated']
+    values = [[1,
+               'test_first_name',
+               'test_last_name',
+               'test_department_id',
+               'test_email',
+               'test_dept_name',
+               'test_location',
+               'test_manager',
+               '2023-11-09 00:00:00',
+               '2023-11-09 00:00:00'],
+              [2,
+               'test_first_name2',
+               'test_last_name2',
+               'test_department_id2',
+               'test_email2',
+               'test_dept_name2',
+               'test_location2',
+               'test_manager2',
+               '2023-11-09 00:00:00',
+               '2023-11-09 00:00:00']]
     return pd.DataFrame(values, columns=column_headers)
 
 
@@ -16,10 +44,15 @@ def test_should_return_a_dataframe(table_data):
 
 
 def test_should_return_all_columns(table_data):
-    expected = ['staff_id', 'first_name', 'last_name', 'department_name', 'location', 'email_address']
+    expected = ['staff_id',
+                'first_name',
+                'last_name',
+                'department_name',
+                'location',
+                'email_address']
     result = list(create_dim_staff(table_data))
     assert expected == result
-    
+
 
 def test_should_raise_key_error_if_passed_bad_data():
     bad_data = pd.DataFrame(['bad_data'])
